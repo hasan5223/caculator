98c50d41ba1a9fdc7d681f265d29e6fcec432fc5
ibrahim hasan abdurrahman <abdurrahmanibrahimhasan@gmail.com>
2024-12-25 21:46:13 +0700

    first commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){
+    int num = 4;
+    if(num > 5){
+    print('Hello, world')
+    }
+    else{
+        print('Else')
+    }
+}
+
+getInfo (String name, int rollNo){
+    print(name);
+    print(rollNo)
+}
